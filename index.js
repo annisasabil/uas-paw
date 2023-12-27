a0314ee65ca71a4b1b53d370b0ccebf59d634c1e
annisasabil <annisasabil>
2023-12-27 11:17:20 +0700

    update code

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const express = require('express');
 const homeRoute = require('./routes/home');
 const mongoose = require('mongoose');
 const app = express();
-const port = 5000;
-const uri = 'mongodb+srv://annisasabila20:2IT0ComOQdQ9UkFW@praktikumweb.jg5kpx2.mongodb.net/?authSource=PraktikumWeb&authMechanism=SCRAM-SHA-1';
+const port = process.env.port || 5000;
 const bodyParser = require('body-parser');
+require('dotenv').config();
 
-mongoose.connect(uri, { 
+mongoose.connect(process.env.MONGO_URI, { 
     useNewUrlParser: true, 
     useUnifiedTopology: true 
 });
